e545add6ae3716da1139a8d6b210f3f8f254f8ea
Jeffrey <jrondeau@degdigital.com>
2017-02-16 10:47:43 -0600

    Helix rename

---
+++ src/Elision.Feature.Template.Accordion/Areas/ElisionFeature/Controllers/AccordionController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+using Sitecore.Mvc.Controllers;
+
+namespace Elision.Feature.Template.Accordion.Areas.ElisionFeature.Controllers
+{
+    public class AccordionController : SitecoreController
+    {       
+        public ActionResult Accordion()
+        {
+            return View();
+        }
+
+        public ActionResult Section()
+        {
+            return View();
+        }
+    }
+}
